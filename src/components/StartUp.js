55c652f3d188e3a1daad236011fddcd298f857c2
AnnahGerletti <agerletti@gmail.com>
2018-01-05 12:01:57 +1300

    settingup environment

---
+++ src/components/StartUp.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class StartUp extends Component {
+  render() {
+    return (
+      <div>redo of tutorial </div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import StartUp from './components/StartUp'
+
+
+ReactDOM.render(<StartUp />, document.querySelector('.container'));
