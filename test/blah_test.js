3e813d08581a100ad13d4f217c2a6fa00a2c5790
AnnahGerletti <agerletti@gmail.com>
2018-01-12 11:11:13 +1300

    figuring out generator functions

--- test/blah_test.js
+++ test/blah_test.js
@@ -4,8 +4,8 @@ Feature('Search for Video');
 Scenario('User search for new video', (I) => {
   const searchTerm = 'cats'
 
-  I.amOnPage('https://localhost8080')
+  I.amOnPage('https://localhost:8080')
   I.seeInTitle('Video Player')
   I.fillField('.video-input', serchTerm)
-  I.see(videos, '.videoList')
+  I.see(videos, '.videoList')//see list of searched videos on side of page
 });
--- test/test-1_test.js
+++ test/test-1_test.js
@@ -1,6 +1,11 @@
 
 Feature('title');
 
-Scenario('Can user see page title', (I) => {
-  I.seeInTitle('Video Player')
+*Scenario('Can user see page title', (I) => {
+  let title =  yield I.grabTitle()
+  I.amOnPage('http://localhost:8080/')
+  I.seeInTitle()
 });
+
+
+// let title = yield I.grabTitle()
