7001a1060edb2f69bf51400ef9aec5879a5272d4
AnnahGerletti <agerletti@gmail.com>
2018-01-17 10:31:02 +1300

    added a global variable file

--- test/blah_test.js
+++ test/blah_test.js
@@ -1,11 +1,11 @@
-
-Feature('Search for Video');
+const globals = require('../globals.json')
+Feature('Search for Video')
 
 Scenario('User search for new video', (I) => {
   const searchTerm = 'cats'
 
-  I.amOnPage('https://localhost:8080')
+  I.amOnPage(globals.pageUrl)
   I.seeInTitle('Video Player')
-  I.fillField('.video-input', serchTerm)
-  I.see(videos, '.videoList')//see list of searched videos on side of page
+  I.fillField('.video-input', searchTerm)
+  I.see('.videoList')//see list of searched videos on side of page
 });
